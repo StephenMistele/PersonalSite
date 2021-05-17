68d5df4439f3e281af4ab62e1e361ee098441fe1
StephenMistele <34781493+StephenMistele@users.noreply.github.com>
2021-05-17 00:23:08 -0700

    Begin career page

--- src/pages/Career.vue
+++ src/pages/Career.vue
@@ -3,25 +3,48 @@
     <div class="page-header page-header-small">
       <parallax
         class="page-header-image"
-        style="background-image: url('img/hackathonimg111.jpg')"
+        style="background-image: url('img/1speech.png')"
       >
       </parallax>
       <div class="content-center">
         <div class="container">
-          <h1 class="title">This page is currently under maintinence</h1>
+          <h1 class="title">Career Development</h1>
           <div class="text-center">
             <a
               href="https://webpages.scu.edu/ftp/smistele/"
               class="btn btn-primary btn-round"
               target="_blank"
             >
-              <b>But you can visit my site!</b>
+              <b>Visit My Website</b>
             </a>
           </div>
         </div>
       </div>
     </div>
     <div class="separator separator-primary"></div>
+    <div class="section section-about-us">
+      <div class="container">
+        <div class="row">
+          <div class="col-md-8 ml-auto mr-auto text-center">
+            <h2 class="title">A Passion for Teaching</h2>
+            <h5 class="description">
+              Starting in high school when I joined 'Coding With Kids', I've found a passion for sharing my techincal knowledge with others. 
+              I spent years as a lead instructor teaching programming concepts to students of all ages, and even started teaching some of my own
+               programming camps independently during summers. Since then I've continued to pursue this passion through educating my fellow peers about 
+               internship opportunities, resume tips, and personal projects. 
+               <br>
+               <br>
+               This is what inspired me to make a 'Career Development' site that helps fellow students interested in computers find resources to 
+               succeed in the workplace.
+               <br>
+               <br>
+               Check out my Site!
+            </h5>
+          </div>
+        </div>
+        <div class="separator separator-primary"></div>
+      </div>
+    </div>
   </div>
 </template>
 <script>
