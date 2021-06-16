ca63c5bacf8b8389cad0484ff898c563afb06f76
StephenMistele <34781493+StephenMistele@users.noreply.github.com>
2021-06-15 19:08:40 -0700

    Revert "Implement contact me functionanlity"
    This reverts commit f7e5b2390b3a2dab680af520fde8162a57252298.

--- src/pages/ACM.vue
+++ src/pages/ACM.vue
@@ -1,15 +1,9 @@
 <template>
   <div>
     <div class="page-header page-header-small">
-      <Particles
-                id="tsparticles"
-                :particlesInit="particlesInit"
-                :particlesLoaded="particlesLoaded"
-                url="http://foo.bar/particles.json"
-        />
       <parallax
         class="page-header-image"
-        style="background-image: url('img/hackforhumanity.jpg')"
+        style="background-image: url('img/hackathonimg111.jpg')"
       >
       </parallax>
       <div class="content-center">
@@ -75,8 +69,6 @@
 </template>
 <script>
 import { Button, FormGroupInput } from "@/components";
-import Particles from "particles.vue";
-Vue.use(Particles);
 export default {
   name: "landing",
   bodyClass: "landing-page",
