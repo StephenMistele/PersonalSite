77475e542c851bc5b0625e7b063a98cc0d8f3f69
StephenMistele <34781493+StephenMistele@users.noreply.github.com>
2021-06-27 18:35:05 -0700

    Finish internship page

--- src/pages/Internships.vue
+++ src/pages/Internships.vue
@@ -3,7 +3,7 @@
     <div class="page-header page-header-small">
       <parallax
         class="page-header-image"
-        style="background-image: url('img/hackathonimg111.jpg')"
+        style="background-image: url('img/intern.jfif')"
       >
       </parallax>
       <div class="content-center">
@@ -26,10 +26,10 @@
       <div class="container">
         <div class="row">
           <div class="col-md-10 ml-auto mr-auto text-center">
-            <h2 class="title">Full Stack Developer and PM</h2>
+            <h2 class="title">Full Stack Developer and TPM</h2>
             <h5 class="description">
               When developing <a href="#/Panterix">PANTERIX</a>, we needed accident data to build our product. I managed to secure this by 
-              building a partnership with <a href="" target="_blank">INRIX</a> to supply the data. While negotiating for this accident data,
+              building a partnership with <a href="https://inrix.com/" target="_blank">INRIX</a> to supply the data. While negotiating for this accident data,
               INRIX was so interested in our idea that they wanted to develop a similar danger product for themselves. We offered to build it, and
               they hired myself and the rest of PANTERIX as interns. At INRIX, I worked on everything from parsing through millions of 
               rows of data in an AWS RDS PostgreSQL DB - to developing the danger algorithm itself - to distributing this data through ASP DOTNET REST API's
@@ -37,12 +37,12 @@
               <br>
               <br>
               More recently I was given a return offer from INRIX to lead the next team of 15+ interns in continuing developing for 
-              their various <a href="" target="_blank">SaaS modules</a> as a technical PM. I'm hoping to use this opportunity to 
+              their various <a href="https://iq.dev.inrix.com/" target="_blank">SaaS modules</a> as a technical PM. I'm hoping to use this opportunity to 
               continue building my leadership skills and honing my coding abilities.
               <br>
               <br>
               In the past I've also interned for the Discovery Institute to build a mobile app in React Native and taught as a full-time instructor for 
-              <a href="" target="_blank">Coding With Kids</a>.
+              <a href="https://www.codingwithkids.com/" target="_blank">Coding With Kids</a>.
             </h5>
           </div>
         </div>
